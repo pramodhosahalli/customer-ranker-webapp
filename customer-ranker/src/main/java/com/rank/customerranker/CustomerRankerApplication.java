dd4005ffc35085c420eb78c1b19b7be1ae5aff6f
Pramod K <pramokv@amazon.com>
2022-08-02 11:59:35 +0530

    Base Project Commit

---
+++ customer-ranker/src/main/java/com/rank/customerranker/CustomerRankerApplication.java
@@ -0,0 +1,13 @@
+package com.rank.customerranker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRankerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRankerApplication.class, args);
+	}
+
+}
---
+++ customer-ranker/src/test/java/com/rank/customerranker/CustomerRankerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rank.customerranker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRankerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
